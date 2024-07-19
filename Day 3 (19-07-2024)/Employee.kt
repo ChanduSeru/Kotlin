252c7c998133a32adc08f33bed8339a71e2468aa
chand1109 <chanduseru0911@gmail.com>
2024-07-19 12:22:51 +0530

    kotlin

---
+++ Day 3 (19-07-2024)/Employee.kt
@@ -0,0 +1,81 @@
+class Employee(
+var id: Int,
+var name: String,
+var department: String,
+var salary: Double){
+    override fun toString(): String{
+        return "Employee[id=$id, name=$name, department=$department, salary=$salary]"
+    }
+    
+}
+class EmployeeManager{
+    private val employees = mutableListOf<Employee>()
+    private var currId = 1
+    
+    //Create New Employee
+    fun createEmployee(name: String, department: String,salary: Double){
+        val newEmployee = Employee(currId++,name,department,salary)
+        employees.add(newEmployee)
+        println("New Employee Created")
+    }
+    //update Existing function
+    fun updateEmployee(id: Int,name: String, department: String,salary: Double){
+        val index = findEmployeeIndexById(id)
+        if(index != -1){
+            employees[index].apply{
+                this.name = name
+                this.department = department
+                this.salary = salary
+            }
+            println("Employee details updated")
+        }else{
+             println("Employee with ID $id not found.")
+        }
+    }
+    //Delete Employee
+    fun deleteEmployee(id: Int){
+        val index = findEmployeeIndexById(id)
+        if(index != 1){
+            employees.removeAt(index)
+            println("Employee Deleted Successfully")
+        }
+        else{
+            println("Employee with ID $id not found.")
+        }
+    }
+    //Show All Employees
+    fun showAllEmployees(){
+        if(employees.isEmpty()){
+            println("Empty")
+        }
+        else{
+            println("Employee List")
+            employees.forEach{
+                println(it)
+            }
+        }
+    }
+    //find employee by id
+    private fun findEmployeeIndexById(id: Int): Int{
+        for ((index, employee) in employees.withIndex()){
+            if(employee.id == id){
+                return index
+            }
+        }
+        return -1
+    }
+    
+}
+
+fun main(){
+    val emp = EmployeeManager();
+    emp.createEmployee("chan", "software", 60000000.0)
+    emp.createEmployee("chand", "Development", 70000000.0)
+    
+    emp.showAllEmployees()
+    //updating
+    emp.updateEmployee(1, "Ram", "SAP", 11000000.0)
+    emp.showAllEmployees()
+    emp.deleteEmployee(1)
+    emp.showAllEmployees()    
+}
